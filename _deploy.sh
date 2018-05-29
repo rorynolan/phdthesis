9e20005a83b00a30ec053b4c3164e58aaae384b0
Rory Nolan <rorynoolan@gmail.com>
2018-05-29 21:32:43 +0100

    Attempt 6 for travis.

--- _deploy.sh
+++ _deploy.sh
@@ -3,5 +3,5 @@ git config --global user.email "rorynoolan@gmail.com"
 git config --global user.name "Rory Nolan"
 git add .
 git commit -m "Update the book"
-git remote add origin-pages https://${GITHUB_PAT}@github.com/rorynolan/phdthesis.git > /dev/null 2>&1
+git remote add origin https://${GITHUB_PAT}@github.com/rorynolan/phdthesis.git > /dev/null 2>&1
 git push --quiet --set-upstream origin master
