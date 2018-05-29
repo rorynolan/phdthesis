a932dcc258eea22939c727b849170f04dbc30e06
Rory Nolan <rorynoolan@gmail.com>
2018-05-29 16:51:25 +0100

    Attempt 3 for travis.

--- _deploy.sh
+++ _deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-git add --all *
+git config --global user.email "rorynoolan@gmail.com"
+git config --global user.name "Rory Nolan"
+git add .
 git commit -m "Update the book"
 git push -q origin master
