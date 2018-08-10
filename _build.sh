7de16c0d3c20d4acd50683b56c231e2ba84f733f
Rory Nolan <rorynoolan@gmail.com>
2018-08-10 16:43:12 +0100

    Printing :-)

--- _build.sh
+++ _build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', paste0('bookdown::', c('gitbook', 'pdf_book', 'epub_book')))"
